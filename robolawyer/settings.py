808db1dbdac60e895a20189928e99573f7e357d9
Aman <aman54kumar@gmail.com>
2020-06-07 23:32:36 +0530

    condition on allowed_hosts

--- robolawyer/settings.py
+++ robolawyer/settings.py
@@ -27,10 +27,12 @@ TEMPLATE_DEBUG = DEBUG
 # Application definition
 SECRET_KEY = config('SECRET_KEY')
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+else:
+    ALLOWED_HOSTS = ['.justbot.eu-central-1.elasticbeanstalk.com']
 
-
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 EMAIL_HOST=config('EMAIL_HOST')
 EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')

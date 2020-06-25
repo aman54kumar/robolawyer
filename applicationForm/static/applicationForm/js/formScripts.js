3b8438226b4ca9cf97e97714ea450539c86fc5c3
Aman <aman54kumar@gmail.com>
2020-06-25 17:17:13 +0530

    st of facts popup editing

--- applicationForm/static/applicationForm/js/formScripts.js
+++ applicationForm/static/applicationForm/js/formScripts.js
@@ -211,10 +211,14 @@ function textCounter(field, field2, maxlimit) {
   var countfield = document.getElementById(field2);
   if (field.value.length > maxlimit) {
     if (field.id === "stofFacts") {
+      var popUpStFactText = document.createElement("div");
+      popUpStFactText.style.textAlign = "justify";
+      popUpStFactText.innerHTML =
+        "You have reached the page limit imposed by the Court. It is possible for you to add a supplementary statement expanding on the facts, complaints or remedies used. This extra statement should not be more than 20 pages. It should not add new complaints or violations but only develop what is already set out in the form. <br/>You can either go back and rephrase your Statement of the facts to comply with the page limit, or you can add extra pages on the Subject matter of the application. Before adding extra pages, make sure that all the central facts are already mentioned in the main Statement of Facts and that you are not adding any additional information, but merely expanding on the already mentioned facts, violations and complaints.";
       swal({
         buttons: ["Go Back", "Add Supplementary Statement"],
-        text:
-          "You have reached the page limit imposed by the Court. It is possible for you to add a supplementary statement expanding on the facts, complaints or remedies used. This extra statement should not be more than 20 pages. It should not add new complaints or violations but only develop what is already set out in the form. You can either go back and rephrase your Statement of the facts to comply with the page limit, or you can add extra pages on the Subject matter of the application. Before adding extra pages, make sure that all the central facts are already mentioned in the main Statement of Facts and that you are not adding any additional information, but merely expanding on the already mentioned facts, violations and complaints.",
+        closeOnClickOutside: false,
+        content: popUpStFactText,
       });
       $(".extraWritingArea").removeClass("is-hidden");
     }

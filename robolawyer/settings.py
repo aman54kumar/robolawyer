cf4ccbad1765386891cf7c575df0f625094a72cf
Aman <aman54kumar@gmail.com>
2020-10-27 13:37:51 +0530

    removed excel attachment

--- robolawyer/settings.py
+++ robolawyer/settings.py
@@ -82,6 +82,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # "compression_middleware.middleware.CompressionMiddleware",
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]

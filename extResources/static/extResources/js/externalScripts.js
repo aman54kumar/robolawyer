0ac2284a36feda3fcc61361ef325d733f1332872
Aman <aman54kumar@gmail.com>
2021-02-22 20:57:31 +0530

    Expand -> Details

--- extResources/static/extResources/js/externalScripts.js
+++ extResources/static/extResources/js/externalScripts.js
@@ -2,15 +2,15 @@ $(".extPButton").on("click", function () {
   // console.log($(this));
   // console.log($(this).html());
   // console.log(
-  //   $(this).html().trim() === 'Expand <i  class="fa fa-angle-right"></i>'
+  //   $(this).html().trim() === 'Expand <i class="fa fa-angle-right"></i>'
   // );
   // $(this).children("i").toggleClass("fa-angle-right");
   // $(this).children("i").toggleClass("fa-angle-down");
   $(this).html(
     $(this).text().toLowerCase() ==
-      'Expand <i class="fa fa-angle-right"></i>'.toLowerCase()
+      'Details <i class="fa fa-angle-right"></i>'.toLowerCase()
       ? 'Collapse <i class="fa fa-angle-down"></i>'.toLowerCase()
-      : 'Expand <i class="fa fa-angle-right"></i>'.toLowerCase()
+      : 'Details <i class="fa fa-angle-right"></i>'.toLowerCase()
   );
 
   // $(this).html(

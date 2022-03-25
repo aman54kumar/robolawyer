5d893c576f386ad985264e957edcc0acfb7f2086
Kumar Aman <aman54kumar@gmail.com>
2022-03-26 03:19:20 +0530

    new sheet update problem.

--- applicationForm/dataPreparation/countryAPI/countryJSON.py
+++ applicationForm/dataPreparation/countryAPI/countryJSON.py
@@ -10,29 +10,30 @@ from datetime import datetime
 from django.conf import settings
 import os
 import jsons
+
 jsons.suppress_warnings()
 
 
 def getFormattedArticleText(text):
-    text = text.split('<br/>\n')
+    text = text.split("<br/>\n")
     if len(text) == 1:
-        return [{'mainText': text, 'points': []}]
+        return [{"mainText": text, "points": []}]
     res = []
     flag = 0
-    temp = {'mainText': '', 'points': []}
+    temp = {"mainText": "", "points": []}
     for i in text:
-        if i[0] != '(':
+        if i[0] != "(":
             # main text
             if flag == 1 or flag == 2:
                 res.append(temp)
-                temp = {'mainText': '', 'points': []}
+                temp = {"mainText": "", "points": []}
             flag = 1
-            temp['mainText'] = i
+            temp["mainText"] = i
 
         else:
             # points
             flag = 2
-            temp['points'].append(i)
+            temp["points"].append(i)
     if flag == 2 or flag == 1:
         res.append(temp)
     return res
@@ -41,21 +42,22 @@ def getFormattedArticleText(text):
 articleFile = os.path.join(
     settings.BASE_DIR,
     # 'applicationForm\\dataPreparation\\countryAPI\\API ratification territorial application reservations - With UK updated.xlsx'
-    'applicationForm/dataPreparation/countryAPI/API ratification territorial application reservations - With UK updated-Jul23.xlsx'
+    "applicationForm/dataPreparation/countryAPI/API ratification territorial application reservations - With UK updated-Jul23.xlsx",
 )
 
 courtFile = os.path.join(
     settings.BASE_DIR,
-    'applicationForm/dataPreparation/countryAPI/Countries_and_national_courts.csv'
+    "applicationForm/dataPreparation/countryAPI/Countries_and_national_courts.csv",
 )
 
 outputFile = os.path.join(
     settings.BASE_DIR,
-    'applicationForm/static/applicationForm/apiFiles/countryArticle.json')
+    "applicationForm/static/applicationForm/apiFiles/countryArticle.json",
+)
 
 tempFile = os.path.join(
-    settings.BASE_DIR,
-    'applicationForm/static/applicationForm/apiFiles/temp.csv')
+    settings.BASE_DIR, "applicationForm/static/applicationForm/apiFiles/temp.csv"
+)
 final_list = {}
 country_json = {}
 article_json = {}
@@ -69,73 +71,76 @@ data = pd.read_excel(articleFile, sheet_name=sheet_name, index_col=0)
 court = pd.read_csv(courtFile, header=0, sep=",", encoding="utf-8")
 
 for element in data:
-    data[element]['Date'] = data[element]['Date'].fillna(fill_dt)
-    data[element]['Active/not active'] = data[element][
-        'Active/not active'].fillna(fill_na)
-    data[element]['Reservations/Derogations/Declarations'] = data[element][
-        'Reservations/Derogations/Declarations'].fillna(fill_na)
-    data[element]['Article'] = data[element]['Article'].fillna(fill_na)
-    data[element]['Full text'] = data[element]['Full text'].fillna(fill_na)
+    data[element]["Date"] = data[element]["Date"].fillna(fill_dt)
+    data[element]["Active/not active"] = data[element]["Active/not active"].fillna(
+        fill_na
+    )
+    data[element]["Reservations/Derogations/Declarations"] = data[element][
+        "Reservations/Derogations/Declarations"
+    ].fillna(fill_na)
+    data[element]["Article"] = data[element]["Article"].fillna(fill_na)
+    data[element]["Full text"] = data[element]["Full text"].fillna(fill_na)
 
-textArray = data[1]['Full text']
+textArray = data[1]["Full text"]
 resultList = []
 for text in textArray:
     resultList.append(getFormattedArticleText(text))
-court_country = court['Country']
+court_country = court["Country"]
 for item in range(1, len(data) + 1):
-    country_json[data[item]['Country'][1]] = {}
+    country_json[data[item]["Country"][1]] = {}
 
 for item in country_json:
     for record in data:
-        if item == data[record]['Country'].any():
-            country_json[item]['Date'] = pd.to_datetime(
-                data[record]['Date']).dt.strftime("%d-%m-%Y")
-            country_json[item]['Active'] = data[record]['Active/not active']
-            country_json[item]['Reservations'] = data[record][
-                'Reservations/Derogations/Declarations']
-            country_json[item]['ratDate'] = pd.to_datetime(
-                data[record]['Date'].min()).strftime("%d-%m-%Y")
-        if item == data[record]['Country'].all():
-            article_json['Article'] = data[record]['Article']
-            article_json['Full text'] = resultList
+        # if item == data[record]["Country"].any():
+        #     country_json[item]["Date"] = pd.to_datetime(
+        #         data[record]["Date"]
+        #     ).dt.strftime("%d-%m-%Y")
+        #     country_json[item]["Active"] = data[record]["Active/not active"]
+        #     country_json[item]["Reservations"] = data[record][
+        #         "Reservations/Derogations/Declarations"
+        #     ]
+        #     country_json[item]["ratDate"] = pd.to_datetime(
+        #         data[record]["Date"].min()
+        #     ).strftime("%d-%m-%Y")
+        if item == data[record]["Country"].all():
+            print("here")
+            article_json["Article"] = data[record]["Article"]
+            article_json["Full text"] = resultList
+        # if item in court["Country"].tolist():
+        #     myIndex = court_country[court_country == item].index[0]
+        #     if (
+        #         type(court["ProceedingType3"].iloc[myIndex]) == float
+        #         and type(court["ProceedingType2"].iloc[myIndex]) == float
+        #     ):
+        #         country_json[item]["Court"] = {
+        #             "ProceedingType1": str(court["ProceedingType1"].iloc[myIndex]),
+        #             "Court1": str(court["Court1"].iloc[myIndex]),
+        #         }
 
-        if item in court['Country'].tolist():
-            myIndex = court_country[court_country == item].index[0]
-            if type(court['ProceedingType3'].iloc[myIndex]) == float and type(
-                    court['ProceedingType2'].iloc[myIndex]) == float:
-                country_json[item]["Court"] = {
-                    'ProceedingType1':
-                    str(court['ProceedingType1'].iloc[myIndex]),
-                    'Court1': str(court['Court1'].iloc[myIndex])
-                }
+        #     elif (
+        #         type(court["ProceedingType3"].iloc[myIndex]) == float
+        #         and type(court["ProceedingType2"].iloc[myIndex]) == str
+        #     ):
 
-            elif type(
-                    court['ProceedingType3'].iloc[myIndex]) == float and type(
-                        court['ProceedingType2'].iloc[myIndex]) == str:
-
-                country_json[item]["Court"] = {
-                    'ProceedingType1':
-                    str(court['ProceedingType1'].iloc[myIndex]),
-                    'Court1': str(court['Court1'].iloc[myIndex]),
-                    'ProceedingType2':
-                    str(court['ProceedingType2'].iloc[myIndex]),
-                    'Court2': str(court['Court2'].iloc[myIndex])
-                }
-            else:
-                country_json[item]["Court"] = {
-                    'ProceedingType1':
-                    str(court['ProceedingType1'].iloc[myIndex]),
-                    'Court1': str(court['Court1'].iloc[myIndex]),
-                    'ProceedingType2':
-                    str(court['ProceedingType2'].iloc[myIndex]),
-                    'Court2': str(court['Court2'].iloc[myIndex]),
-                    'ProceedingType3':
-                    str(court['ProceedingType3'].iloc[myIndex]),
-                    'Court3': str(court['Court3'].iloc[myIndex])
-                }
+        #         country_json[item]["Court"] = {
+        #             "ProceedingType1": str(court["ProceedingType1"].iloc[myIndex]),
+        #             "Court1": str(court["Court1"].iloc[myIndex]),
+        #             "ProceedingType2": str(court["ProceedingType2"].iloc[myIndex]),
+        #             "Court2": str(court["Court2"].iloc[myIndex]),
+        #         }
+        #     else:
+        #         country_json[item]["Court"] = {
+        #             "ProceedingType1": str(court["ProceedingType1"].iloc[myIndex]),
+        #             "Court1": str(court["Court1"].iloc[myIndex]),
+        #             "ProceedingType2": str(court["ProceedingType2"].iloc[myIndex]),
+        #             "Court2": str(court["Court2"].iloc[myIndex]),
+        #             "ProceedingType3": str(court["ProceedingType3"].iloc[myIndex]),
+        #             "Court3": str(court["Court3"].iloc[myIndex]),
+        #         }
 
 final_list["country"] = country_json
 final_list["article"] = article_json
 
-with open(outputFile, 'w', encoding="UTF-8") as ps:
-    ps.write(jsons.dumps(final_list))
+# print(final_list["article"])
+# with open(outputFile, "w", encoding="UTF-8") as ps:
+#     ps.write(jsons.dumps(final_list))

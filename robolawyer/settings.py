a857dced18ac8783c6177869097a9c4c492095a1
Kumar Aman <aman54kumar@gmail.com>
2022-03-31 00:47:52 +0530

    logging implemented, sentry with raven set up

--- robolawyer/settings.py
+++ robolawyer/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "raven.contrib.django.raven_compat",
     "storages",
     "corsheaders",
     "svglib",
@@ -232,5 +233,24 @@ STATICFILES_FINDERS = (
 )
 
 # LOGGING
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+RAVEN_CONFIG = {
+    "dsn": "https://cde5f0df67d04df3b44049379526e042@o1183119.ingest.sentry.io/6300191",
+}
+
+# sentry_sdk.init(
+#     dsn="https://cde5f0df67d04df3b44049379526e042@o1183119.ingest.sentry.io/6300191",
+#     integrations=[DjangoIntegration()],
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for performance monitoring.
+#     # We recommend adjusting this value in production.
+#     traces_sample_rate=1.0,
+#     # If you wish to associate users to errors (assuming you are using
+#     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii=True,
+# )
 
 LOGGING = LOGGING

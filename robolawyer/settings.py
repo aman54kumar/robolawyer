3aa21fe834209edc0661d6bea2d5f8fb02d9347e
Kumar Aman <aman54kumar@gmail.com>
2022-03-31 00:50:10 +0530

    removed commented lines

--- robolawyer/settings.py
+++ robolawyer/settings.py
@@ -234,23 +234,9 @@ STATICFILES_FINDERS = (
 
 # LOGGING
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
 
 RAVEN_CONFIG = {
     "dsn": "https://cde5f0df67d04df3b44049379526e042@o1183119.ingest.sentry.io/6300191",
 }
-
-# sentry_sdk.init(
-#     dsn="https://cde5f0df67d04df3b44049379526e042@o1183119.ingest.sentry.io/6300191",
-#     integrations=[DjangoIntegration()],
-#     # Set traces_sample_rate to 1.0 to capture 100%
-#     # of transactions for performance monitoring.
-#     # We recommend adjusting this value in production.
-#     traces_sample_rate=1.0,
-#     # If you wish to associate users to errors (assuming you are using
-#     # django.contrib.auth) you may enable sending PII data.
-#     send_default_pii=True,
-# )
 
 LOGGING = LOGGING
